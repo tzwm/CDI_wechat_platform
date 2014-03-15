8f28d9aebb4d4ee31a8481b2e6a49f79d060c821
Samwise Wang <tzwm@tzwm-debian>
2014-03-16 04:45:23 +0800

    Add index.html to simply display message from wechat by socket.io.

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ var TOKEN = 'cdi';
 var restify = require('restify');
 var crypto = require('crypto');
 var xml2js = require('xml2js');
+var fs = require('fs');
 
 var server = restify.createServer();
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
+var io = require('socket.io').listen(server);
+
+
 server.get('/wechat', auth);
 server.post('/wechat', receiveMsg);
 
-server.listen(PORT, function() {
-      console.log('%s listening at %s', server.name, server.url);
-});
+server.get('/index', index);
+
 
 function auth(req, res, next) {
     var signature = req.params.signature;
@@ -44,10 +47,27 @@ function receiveMsg(req, res, next) {
     var parser = new xml2js.Parser();
     parser.parseString(req.body, function(err, result) {
         console.dir(result);
+
+        io.sockets.emit('msg-text', {FromUserName: result.xml.FromUserName, Content: result.xml.Content});
     });
 
     res.setHeader('content-type', 'text/plain');
     res.send(200, '');
     return next();
 }
+
+function index(req, res, next) {
+    res.writeHead(200, {'content-type': 'text/html'});
+    fs.readFile('./index.html', 'utf-8', function(err, text){
+        res.write(text);
+        res.end();
+    });
+
+    return next();
+}
+
+
+server.listen(PORT, function() {
+      console.log('%s listening at %s', server.name, server.url);
+});
 
